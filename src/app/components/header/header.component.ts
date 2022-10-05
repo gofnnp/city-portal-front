de46f891095677a68fde37d8c640edda987424df
ggbdfb <fuvdvf@uhdvcf.com>
2022-10-05 13:22:49 +0400

    ipyuk

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -12,20 +13,24 @@ export class HeaderComponent implements OnInit {
   @Input() authPage!: string;
   @Input() showAuthModel: boolean = false;
   signUpForm!: FormGroup;
+  signInForm!: FormGroup;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: HttpClient) {
     this.signUpForm = new FormGroup({
-
-      "userLastname": new FormControl("", Validators.required),
-      "userName": new FormControl("", Validators.required),
-      "userOtch": new FormControl(""),
-      "userLogin": new FormControl("", Validators.required),
-      "userEmail": new FormControl("", [
+      "last_name": new FormControl("", Validators.required),
+      "first_name": new FormControl("", Validators.required),
+      "otch": new FormControl(""),
+      "login": new FormControl("", Validators.required),
+      "email": new FormControl("", [
         Validators.required,
         Validators.email
       ]),
-      "userPass": new FormControl("", [Validators.required, Validators.minLength(8)]),
+      "password": new FormControl("", [Validators.required, Validators.minLength(8)]),
       "userRepass": new FormControl("", [Validators.required, Validators.minLength(8)])
+    });
+    this.signInForm = new FormGroup({
+      "login": new FormControl("", Validators.required),
+      "password": new FormControl("", [Validators.required, Validators.minLength(8)])
     });
   }
 
@@ -45,7 +50,26 @@ export class HeaderComponent implements OnInit {
     const userData = this.signUpForm.value
     console.log(userData);
     
+    this.http.post('http://78.85.18.10:5005/add_user', userData).subscribe(
+      {
+        next: (data: any) => console.log(data),
+        error: (error) => console.error(error)
+      }
+    );
+  }
 
+  signIn() {
+    if (this.signInForm.invalid) {
+      this.markFormGroupTouched(this.signUpForm)
+      return
+    }
+    const userData = this.signInForm.value
+    this.http.post('http://78.85.18.10:5005/auth', userData).subscribe(
+      {
+        next: (data: any) => console.log(data),
+        error: (error) => console.error(error)
+      }
+    );
   }
 
   changeAuthPage(page: string) {
